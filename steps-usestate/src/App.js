2fd9de7f43da8293029ba5713ba33f516b02a108
Sercan Sebil <sercansebil95@googlemail.com>
2024-11-05 12:40:24 +0100

    Add steps-usestate challenge

---
+++ steps-usestate/src/App.js
@@ -0,0 +1,75 @@
+import "./index.css";
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        style={{
+          backgroundColor: "#7950f2",
+          color: "#fff",
+          padding: "10px 10px",
+          border: "none",
+          borderRadius: "5px",
+          display: "block",
+          margin: "0 auto",
+        }}
+      >
+        {isOpen ? "Close" : "Open"} Steps
+      </button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Prev
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
